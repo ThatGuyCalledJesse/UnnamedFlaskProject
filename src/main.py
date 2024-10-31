35f1b78eec3013f4369395648da6f041da078cc6
Jesse Swart <jesseswart08@gmail.com>
2024-10-31 22:17:32 +0100

    Raspberry pi push

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+import os
+
+app = Flask(__name__, static_folder="static")
+
+print(os.getcwd())
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
