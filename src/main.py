cd4746df5b07bfece9b0e507e97f660dc8878424
Jesse Swart <jesseswart08@gmail.com>
2024-10-31 22:20:33 +0100

    this should work

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 import os
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__, static_folder="static", template_folder=f"{os.getcwd()}/templates")
 
 print(os.getcwd())
 
